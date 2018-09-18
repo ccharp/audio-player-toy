6140b10ffdb471e322ff06f09bf3ff3ef5ca13ff
Corbin Charpentier <corbin.charpentier@gmail.com>
2018-09-18 14:00:15 -0700

    Initial commit

---
+++ audio-player/audio-player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  audio-player
+//
+//  Created by Corbin Charpentier on 9/18/18.
+//  Copyright © 2018 Corbin Charpentier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
